b521cfc8649e57221eb94e50b8151331e60b9663
Anna Kopylova <anna.metisova@gmail.com>
2023-05-17 18:33:00 +0200

    Uloha 2 - Bublina po najeti mysi na obrazek

--- src/Uloha2/index.jsx
+++ src/Uloha2/index.jsx
@@ -4,5 +4,8 @@ import hradUrl from './hrad.svg'
 // Zadání: Po najetí myši, `onMouseEnter` na obrázek zobraz bublinu s textem „Na hradě je myš!“
 
 export const Uloha2 = () => {
-	return <img src={hradUrl} width={60} height={60} alt="hrad" />
+	const poNajeti = () => {
+    alert("Na hradě je myš!");};
+		
+	return <img onMouseEnter={poNajeti} src={hradUrl} width={60} height={60} alt="hrad" />
 }
